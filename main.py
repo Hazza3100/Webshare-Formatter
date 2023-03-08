3ebccd32cd1232746f32d0f13c969f98cfb0b615
Hazza <86918289+Hazza3100@users.noreply.github.com>
2023-03-08 19:20:39 +0000

    Code is approximately 0.0186s faster!
    Code is approximately 0.0186s faster!

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from functools import cache
 
 class Formatter:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     @staticmethod
     @cache
     def format(value):
-        for proxy in open(value, 'r').read().splitlines():
-            data     = proxy.split(':')
-            ip       = data[0]
-            port     = data[1]
-            username = data[2]
-            password = data[3]
-            with open('formatted.txt', 'a') as f:
-                f.write(f'{username}:{password}@{ip}:{port}\n')
+        with open(value) as f:
+            lines = f.read().split('\n')
+        formatted_lines = []
+        for line in lines:
+            if line.strip():
+                ip, port, username, password = line.split(':')
+                formatted_line = '{}:{}@{}:{}\n'.format(username, password, ip, port)
+                formatted_lines.append(formatted_line)
+        with open('formatted.txt', 'w') as f:
+            f.writelines(formatted_lines)
 
 Formatter().format('proxy.txt')
